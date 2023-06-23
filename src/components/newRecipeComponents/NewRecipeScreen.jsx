9cac857bb9ac430c7fb93f840232e3060a52fbe9
Rex Holmes <holmesrex515@gmail.com>
2023-06-24 01:46:49 +0900

    NewRecipeScreen added

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik } from "formik";
+import axios from "axios";
 
 const NewRecipeScreen = () => {
+
+  const [ingredients, setIngredients] = useState([])
+  const [name, setName] = useState("")
+  const [quantity, setQuantity] = useState("")
+
+  const addIngredient = () => {
+    setIngredients([...ingredients, { name, quantity }])
+    setName("")
+    setQuantity("")
+  }
+
+  const initialValues = {
+    type:"",
+    recipeName:"",
+    imageURL:"",
+    prepTime:"",
+    cookTime:"",
+    serves:"",
+    ingredients:[],
+    instructions:"",
+  }
+
+  const onSubmit = (values) => {
+    values.ingredients = ingredients
+    console.log(values)
+  }
+
+  const ingredientDisplay = ingredients.map((ing) => {
+    return (
+      <li>
+        {ing.quantity} {ing.name}
+      </li>
+    )
+  })
+
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ values, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="name-url">
+            <input value={values.recipeName} onChange={handleChange} placeholder="Name" name="recipeName"/>
+            <input value={values.imageURL} onChange={handleChange} placeholder="Image URL" name="imageURL"/>
+          </div>
+          <div className="radio-btns-container">
+            <input type="radio" value="Cook" onChange={handleChange} name="type"/><h5>Cook</h5>
+            <input type="radio" value="Bake" onChange={handleChange} name="type"/><h5>Bake</h5>
+            <input type="radio" value="Drink" onChange={handleChange} name="type"/><h5>Drink</h5>
+          </div>
+          <div className="prep-cook-serve">
+            <input placeholder="Prep Time" onChange={handleChange} value={values.prepTime} name="prepTime" />
+            <input placeholder="Cook Time" onChange={handleChange} value={values.cookTime} name="cookTime" />
+            <input placeholder="Serves" onChange={handleChange} value={values.serves} name="serves" />
+          </div>
+          <h3>Ingredients</h3>
+          <div className="input-container">
+          <div className="ing-quant-container">
+            <input placeholder="Ingredient" value={name} onChange={(event) => setName(event.target.value)}/>
+            <input placeholder="Quantity"  value={quantity} onChange={(event) => setQuantity(event.target.value)}/>
+          </div>
+          <ul>{ingredientDisplay}</ul>
+          </div>
+          <button className="ing-btn" onClick={addIngredient}>Add Another</button>
+          <textarea placeholder="What are the instructions?" onChange={handleChange} value={values.instructions} name="instructions" rows={5}/>
+          <button type="submit" className="blue-btn">Submit</button>
+        </form>
+        )}
+      </Formik>
     </section>
   );
 };
